ae209d5f7e952b6437fa2ac666f86ab12c4ee3d2
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-07-15 10:06:32 -0400

    final commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation;
+
+public class BankAccount
+{ 
+    public BankAccount() 
+{ 
+}
+    private double balance = 0;
+    public void Deposit(double totalAmount)
+    {
+        balance = totalAmount;
+    }
+    public string GetBalance()
+    {
+        return $"${balance}";
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,10 @@
+﻿using BankEncapsulation;
+
+
+var bankAccount = new BankAccount();
+
+Console.Write("Deposit all of your money: ");
+var userInput = double.Parse(Console.ReadLine());
+bankAccount.Deposit(userInput);
+
+Console.Write($"your total is {bankAccount.GetBalance()}");
